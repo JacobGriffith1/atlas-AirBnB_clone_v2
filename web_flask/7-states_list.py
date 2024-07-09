fc2798f53840669f811f2562c45f12914e38d9fd
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-08 19:47:43 -0500

    put 7 stuff with the other methods

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,71 @@
 #!/usr/bin/python3
 """Script that starts a Flask web app"""
 from flask import Flask, render_template
-from models import storage
-from models.state import State
 
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown():
-    """Teardown"""
-    storage.close()
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Displays 'C' and the value of 'text'"""
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text='is cool'):
+    """Displays 'Python' and the value of 'text'"""
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    """Displays 'n is a number' ONLY if n is an integer"""
+    return "%d is a number" % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page ONLY if n is an integer"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def num_odd_even(n):
+    """Displays an HTML page ONLY if n is an integer, and it's odd/even"""
+    return render_template('6-number_odd_or_even.html', number=n)
 
 
 @app.route('/states_list', strict_slashes=False)
 def display_html():
     """Displays HTML page inside the tag BODY"""
+    from models import storage
+    from models.state import State
     states = storage.all(State)
     dict_to_html = {value.id: value.name for value in states.values()}
     return render_template('7-states_list.html',
                            Table="States",
                            items=dict_to_html)
+
+
+@app.teardown_appcontext
+def teardown():
+    """Teardown"""
+    from models import storage
+    storage.close()
 
 
 if __name__ == "__main__":
