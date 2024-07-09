357f51fbc7da1f3a9081b717d682335650f28b09
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-09 18:38:45 -0500

    changed back to how it was before; the previous comit made things worse.

--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
 from models.city import City
 import os
@@ -18,14 +17,13 @@ class State(BaseModel, Base):
         name = ""
         cities = []
 
-    if models.storage_t != "db":
-        @property
-        def cities(self):
-            """Getter for cities"""
-            from models import storage
-            city_list = []
-            all_cities = storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        """Getter for cities"""
+        from models import storage
+        city_list = []
+        all_cities = storage.all(City)
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
